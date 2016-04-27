65658ca285cdb7c64fa6c4063265bce5e059adda
Filippo Valsorda <filippo@cloudflare.com>
2016-04-27 04:50:52 +0100

    First version

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/build"
+	"go/parser"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/kisielk/gotool"
+
+	"golang.org/x/tools/go/loader"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: vendorcheck [-t] package [package ...]")
+		flag.PrintDefaults()
+	}
+	tests := flag.Bool("t", false, "also check dependencies of test files")
+	flag.Parse()
+
+	var conf loader.Config
+	conf.ParserMode = parser.ImportsOnly
+	conf.AllowErrors = true
+	conf.TypeChecker.Error = func(error) {}
+	for _, p := range gotool.ImportPaths(flag.Args()) {
+		if *tests {
+			conf.ImportWithTests(p)
+		} else {
+			conf.Import(p)
+		}
+	}
+	prog, err := conf.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initial := make(map[*loader.PackageInfo]bool)
+	for _, pi := range prog.InitialPackages() {
+		initial[pi] = true
+	}
+
+	var packages []*loader.PackageInfo
+	for _, pi := range prog.AllPackages {
+		if initial[pi] {
+			continue
+		}
+		if len(pi.Files) == 0 {
+			continue // virtual stdlib package
+		}
+		filename := prog.Fset.File(pi.Files[0].Pos()).Name()
+		if !strings.HasPrefix(filename, build.Default.GOROOT) || !isStandardImportPath(pi.Pkg.Path()) {
+			packages = append(packages, pi)
+		}
+	}
+
+	exitCode := 0
+	for _, pi := range packages {
+		if strings.Index(pi.Pkg.Path(), "/vendor/") == -1 {
+			fmt.Println("[!] dependency not vendored:", pi.Pkg.Path())
+			exitCode = 1
+		}
+	}
+	os.Exit(exitCode)
+}
+
+func isStandardImportPath(path string) bool {
+	// from https://github.com/golang/go/blob/87bca88/src/cmd/go/pkg.go#L183-L194
+	i := strings.Index(path, "/")
+	if i < 0 {
+		i = len(path)
+	}
+	elem := path[:i]
+	return !strings.Contains(elem, ".")
+}
